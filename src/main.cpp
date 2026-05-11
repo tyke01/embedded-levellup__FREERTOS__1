ecf8dd3e812f4e561e57c02826292366b377de8c
tyke01 <techvictor653@gmail.com>
2026-05-11 16:18:11 +0300

    update: embedded levelling -- message sharing across tasks: I understand absolutely nothing of this

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,131 @@
 #include <Arduino.h>
 
-// use only 1 core
+// Use only core 1 for demo purposes
 #if CONFIG_FREERTOS_UNICORE
-static const BaseType_t app_cpu = 0;
+  static const BaseType_t app_cpu = 0;
 #else
-static const BaseType_t app_cpu = 1;
+  static const BaseType_t app_cpu = 1;
 #endif
 
+// Settings
+static const uint8_t buf_len = 255;
 
-void testTask (void *parameter) {
+// Globals
+static char *msg_ptr = NULL;
+static volatile uint8_t msg_flag = 0;
+
+//*****************************************************************************
+// Tasks
+
+// Task: read message from Serial buffer
+void readSerial(void *parameters) {
+
+  char c;
+  char buf[buf_len];
+  uint8_t idx = 0;
+
+  // Clear whole buffer
+  memset(buf, 0, buf_len);
+  
+  // Loop forever
   while (1) {
-    int a = 1;
-    int b[100];
 
-    for (int i = 0; i < 100; i++) {
-      b[i] = a + 1;
-    }
-    Serial.println(b[0]);
+    // Read cahracters from serial
+    if (Serial.available() > 0) {
+      c = Serial.read();
 
-    // peint out remaining stack memory in words
-    Serial.print("High water mark (words): ");
-    Serial.println(uxTaskGetStackHighWaterMark(NULL));
+      // Store received character to buffer if not over buffer limit
+      if (idx < buf_len - 1) {
+        buf[idx] = c;
+        idx  ;
+      }
 
-    // Peint out number of free heap memory bytes before malloc
-    Serial.print("Heap before malloc (bytes): ");
-    Serial.println(xPortGetFreeHeapSize());
+      // Create a message buffer for print task
+      if (c == '\n') {
 
-    int *ptr = (int *)pvPortMalloc(1024 * sizeof(int));
+        // The last character in the string is '\n', so we need to replace
+        // it with '\0' to make it null-terminated
+        buf[idx - 1] = '\0';
 
-    // checking heap overflow by checking malloc output
-    if(ptr == NULL) {
-      Serial.println("Not enough heap:");
-    } else {
-      // Do something with the allocated memory so it's not optimized out by the compiler
-      for (int i = 0; i < 1024; i++) {
-        ptr[i] = 3;
+        // Try to allocate memory and copy over message. If message buffer is
+        // still in use, ignore the entire message.
+        if (msg_flag == 0) {
+          msg_ptr = (char *)pvPortMalloc(idx * sizeof(char));
+
+          // If malloc returns 0 (out of memory), throw an error and reset
+          configASSERT(msg_ptr);
+
+          // Copy message
+          memcpy(msg_ptr, buf, idx);
+
+          // Notify other task that message is ready
+          msg_flag = 1;
+        }
+
+        // Reset receive buffer and index counter
+        memset(buf, 0, buf_len);
+        idx = 0;
       }
     }
-
-    // Peint out number of free heap memory bytes after malloc
-    Serial.print("Heap after malloc (bytes): ");
-    Serial.println(xPortGetFreeHeapSize());
-
-
-    // Free up our allocated memory
-    vPortFree(ptr);
-    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
 
-void setup() {
-  Serial.begin(115200);
+// Task: print message whenever flag is set and free buffer
+void printMessage(void *parameters) {
+  while (1) {
 
-  // wait a moment to start
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  Serial.println();
+    // Wait for flag to be set and print message
+    if (msg_flag == 1) {
+      Serial.println(msg_ptr);
 
-  Serial.println("--- FreeRTOS Memory Demo ---");
+      // Give amount of free heap memory (uncomment if you'd like to see it)
+//      Serial.print("Free heap (bytes): ");
+//      Serial.println(xPortGetFreeHeapSize());
 
-  // start the 1 only task
-
-  xTaskCreatePinnedToCore(
-    testTask,   // Task function
-    "TestTask", // Name of the task
-    1500,       // Stack size in words
-    NULL,       // Task input parameter
-    1,          // Priority of the task
-    NULL,       // Task handle
-    app_cpu      // Core where the task should run
-  );
-
-  vTaskDelete(NULL); // delete the setup and loop task
+      // Free buffer, set pointer to null, and clear flag
+      vPortFree(msg_ptr);
+      msg_ptr = NULL;
+      msg_flag = 0;
+    }
+  }
 }
 
-void loop () {
-  // 
+//*****************************************************************************
+// Main (runs as its own task with priority 1 on core 1)
+
+void setup() {
+
+  // Configure Serial
+  Serial.begin(115200);
+
+  // Wait a moment to start (so we don't miss Serial output)
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS Heap Demo---");
+  Serial.println("Enter a string");
+
+  // Start Serial receive task
+  xTaskCreatePinnedToCore(readSerial,
+                          "Read Serial",
+                          1024,
+                          NULL,
+                          1,
+                          NULL,
+                          app_cpu);
+
+  // Start Serial print task
+  xTaskCreatePinnedToCore(printMessage,
+                          "Print Message",
+                          1024,
+                          NULL,
+                          1,
+                          NULL,
+                          app_cpu);
+  
+  // Delete "setup and loop" task
+  vTaskDelete(NULL);
+}
+
+void loop() {
+  // Execution should never get here
 }
